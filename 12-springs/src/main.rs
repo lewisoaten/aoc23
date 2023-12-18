1e37fce7441bab69090b79e9ab4ff9d821377057
Lewis Oaten <lewis@oaten.name>
2023-12-18 11:38:36 +0000

    Twenty-fourth Star

--- 12-springs/src/main.rs
+++ 12-springs/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufRead, BufReader}, env, fs::File};
+use std::{io::{BufRead, BufReader}, env, fs::File, collections::HashMap};
 
 struct MaintenanceRecord {
     springs: Vec<char>,
@@ -70,30 +70,52 @@ impl MaintenanceRecord {
         Ok(maintenance_records)
     }
 
-    fn possible_failures(&self, lava: &[char], springs: &[usize]) -> usize {
+    fn possible_failures<'a>(&self, lava: &'a[char], springs: &'a[usize], cache: &mut Box<HashMap<(&'a [char], &'a [usize]), usize>>) -> usize {
+        
+        if let Some(result) = cache.get(&(lava, springs)) {
+            return *result;
+        }
         let mut result = 0;
 
         if springs.is_empty() {
             return if lava.contains(&'#') { 0 } else { 1 };
         }
 
-        let (current, springs) = (springs[0], &springs[1..]);
-        for i in 0..(lava.len() as usize - springs.iter().sum::<usize>() - springs.len() as usize - current + 1) {
+        let (current, remaining_springs) = (springs[0], &springs[1..]);
+        for i in 0..(lava.len() as usize - remaining_springs.iter().sum::<usize>() - remaining_springs.len() as usize - current + 1) {
             if lava[..(lava.len()).min(i)].contains(&'#') {
                 break;
             }
 
             let next = i + current;
             if next <= lava.len() as usize && !lava[i..(lava.len()).min(next)].contains(&'.') && lava[next..(lava.len()).min(next+1)] != ['#'] {
-                result += self.possible_failures(&lava[(lava.len()).min(next + 1)..], springs);
+                result += self.possible_failures(&lava[(lava.len()).min(next + 1)..], remaining_springs, cache);
             }
         }
+
+        cache.insert((lava, springs), result);
 
         result
     }
 
     fn count_possible_failures(&self) -> usize {
-        self.possible_failures(&self.springs[..], &self.damaged_springs)
+        let mut cache = Box::new(HashMap::new());
+        self.possible_failures(&self.springs[..], &self.damaged_springs, &mut cache)
+    }
+
+    fn count_possible_failures_unfold(&self) -> usize {
+        let mut new_springs = Vec::new();
+        for i in 0..5 {
+            new_springs.extend(&self.springs);
+            if i < 4 {
+                new_springs.push('?');
+            }
+        }
+        let new_damaged_springs = &self.damaged_springs.repeat(5);
+
+        let mut cache = Box::new(HashMap::new());
+
+        self.possible_failures(&new_springs[..], new_damaged_springs, &mut cache)
     }
 }
 
@@ -107,11 +129,17 @@ fn main() {
     let maintenance_record = MaintenanceRecord::parse_all_maintenance_records(reader).expect("Parsed maintenance records");
 
     let mut total_possible_failures = 0;
-    for record in maintenance_record {
+    for record in maintenance_record.iter() {
         total_possible_failures += record.count_possible_failures();
     }
 
+    let mut total_possible_failures_unfold = 0;
+    for record in maintenance_record.iter() {
+        total_possible_failures_unfold += record.count_possible_failures_unfold();
+    }
+
     println!("Total possible failures: {}", total_possible_failures);
+    println!("Total possible failures (unfolded): {}", total_possible_failures_unfold);
     
 }
 
@@ -152,5 +180,33 @@ mod tests {
         assert_eq!(records[3].count_possible_failures(), 1);
         assert_eq!(records[4].count_possible_failures(), 4);
         assert_eq!(records[5].count_possible_failures(), 10);
+
+        let mut total_possible_failures = 0;
+        for record in records.iter() {
+            total_possible_failures += record.count_possible_failures();
+        }
+
+        assert_eq!(total_possible_failures, 21)
+    }
+
+    #[test]
+    fn test_possible_failures_unfold() {
+        let input = test_data();
+        let reader = std::io::Cursor::new(input);
+        let records = MaintenanceRecord::parse_all_maintenance_records(reader).unwrap();
+
+        assert_eq!(records[0].count_possible_failures_unfold(), 1);
+        assert_eq!(records[1].count_possible_failures_unfold(), 16384);
+        assert_eq!(records[2].count_possible_failures_unfold(), 1);
+        assert_eq!(records[3].count_possible_failures_unfold(), 16);
+        assert_eq!(records[4].count_possible_failures_unfold(), 2500);
+        assert_eq!(records[5].count_possible_failures_unfold(), 506250);
+
+        let mut total_possible_failures_unfold = 0;
+        for record in records.iter() {
+            total_possible_failures_unfold += record.count_possible_failures_unfold();
+        }
+
+        assert_eq!(total_possible_failures_unfold, 525152)
     }
 }
