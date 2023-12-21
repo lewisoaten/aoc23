9776a2a8ac109126c14f314232ecbd5c680ef443
Lewis Oaten <lewis@oaten.name>
2023-12-21 09:35:49 +0000

    Thirtieth Star

--- 15-hash/src/main.rs
+++ 15-hash/src/main.rs
@@ -1,7 +1,17 @@
-use std::{io::{BufRead, BufReader}, env, fs::File, collections::HashMap};
+use std::{io::{BufRead, BufReader}, env, fs::File, collections::HashMap, num::{IntErrorKind, ParseIntError}};
 
+#[derive(Debug, Clone)]
+enum Operation {
+    Dash,
+    Equals,
+}
+
+#[derive(Debug, Clone)]
 struct Instruction {
     text: String,
+    label: String,
+    operation: Operation,
+    focul_length: Option<u8>,
 }
 
 struct Init {
@@ -47,9 +57,47 @@ impl From<std::string::FromUtf8Error> for ParseError {
     }
 }
 
+impl PartialEq for Instruction {
+    fn eq(&self, other: &Instruction) -> bool {
+        self.label == other.label
+    }
+
+}
+
 impl Instruction {
-    fn new(text: String) -> Self {
-        Instruction { text }
+    fn parse_instruction(text: String) -> Result<Instruction, ParseError> {
+        let new_text = text.clone();
+        let label_and_operation = text.split_inclusive(['-', '=']).nth(0).expect("No label");
+        let focul_length = text.split(['-', '=']).nth(1).expect("No focul_length");
+
+        let label = &label_and_operation[..label_and_operation.len()-1];
+        let operation = &label_and_operation[label_and_operation.len()-1..label_and_operation.len()]
+            .chars()
+            .nth(0)
+            .expect("Can't strip off operation");
+
+        let operation = match operation {
+            '-' => Operation::Dash,
+            '=' => Operation::Equals,
+            _ => return Err("Invalid operation".into()),
+        };
+
+        let focul_length = match focul_length.parse() {
+            Ok(focal_length) => Some(focal_length),
+            Err(err) => {
+                match (err as ParseIntError).kind() {
+                    IntErrorKind::Empty => None,
+                    _ => panic!("Invalid focul length")
+                }
+            },
+        };
+
+        Ok(Instruction {
+            text: new_text,
+            label: label.to_string(),
+            operation,
+            focul_length,
+        })
     }
 
     fn hash(&self) -> u8 {
@@ -60,6 +108,18 @@ impl Instruction {
 
         let mut hash: u32 = 0;
         for c in self.text.chars() {
+            let ascii = c as u32;
+            hash += ascii;
+            hash *= 17;
+            hash %= 256;
+        }
+
+        hash as u8
+    }
+
+    fn hash_label(&self) -> u8 {
+        let mut hash: u32 = 0;
+        for c in self.label.chars() {
             let ascii = c as u32;
             hash += ascii;
             hash *= 17;
@@ -80,7 +140,9 @@ impl Init {
         for line in reader.split(b',') {
             let line = line?;
 
-            instructions.push(Instruction::new(String::from_utf8(line)?.trim().to_string()));
+            let instruction_string = String::from_utf8(line)?.trim().to_string();
+
+            instructions.push(Instruction::parse_instruction(instruction_string)?);
         }
 
         Ok(Init::new(instructions))
@@ -94,6 +156,52 @@ impl Init {
 
         sum
     }
+
+    fn initialise(&self) -> HashMap<u8, Vec<Instruction>> {
+        let mut lens_boxes: HashMap<u8, Vec<Instruction>> = HashMap::new();
+
+        for instruction in self.sequence.iter() {
+            match instruction.operation {
+                Operation::Dash => {
+                    if let Some(lens_box) = lens_boxes.get_mut(&instruction.hash_label()) {
+                        if let Some(index) = lens_box.iter().position(|x| x == instruction) {
+                            lens_box.remove(index);
+                        }
+                    }
+                },
+                Operation::Equals => {
+                    if let Some(lens_box) = lens_boxes.get_mut(&instruction.hash_label()) {
+                        if let Some(index) = lens_box.iter().position(|x| x == instruction) {
+                                lens_box[index] = (*instruction).clone();
+                        } else {
+                            lens_box.push((*instruction).clone());
+                        }
+                    } else {
+                        let mut lens_box = Vec::new();
+                        lens_box.push((*instruction).clone());
+                        lens_boxes.insert(instruction.hash_label(), lens_box);
+                    }
+                },
+            }
+        }
+
+        lens_boxes
+    }
+
+    fn calculate_focusing_power(lens_boxes: HashMap<u8, Vec<Instruction>>) -> u32 {
+        let mut focusing_power: u32 = 0;
+        for (hash, lens_box) in lens_boxes.iter() {
+            if lens_box.len() >= 1 {
+                for (position, instruction) in lens_box.iter().enumerate() {
+                    if let Some(focal_length) = instruction.focul_length {
+                        focusing_power += (hash.clone() as u32 + 1) * (position as u32 + 1) * focal_length as u32;
+                    }
+                }
+            }
+        }
+
+        focusing_power
+    }
 }
 
 fn main() {
@@ -103,9 +211,13 @@ fn main() {
     let file = File::open(filename).expect("Failed to open file");
     let reader = BufReader::new(file);
 
-    let mut platform = Init::parse_init_sequence(reader).expect("Parsed init sequence");
+    let platform = Init::parse_init_sequence(reader).expect("Parsed init sequence");
 
     println!("Sum of hashes: {}", platform.sum_hashes());
+
+    let lens_boxes = platform.initialise();
+
+    println!("Focusing power: {}", Init::calculate_focusing_power(lens_boxes));
 }
 
 
@@ -118,7 +230,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_patterns() {
+    fn test_parse_init_sequence() {
         let input = test_data();
         let reader = std::io::Cursor::new(input);
         let init = Init::parse_init_sequence(reader).unwrap();
@@ -154,4 +266,27 @@ mod tests {
         assert_eq!(init.sum_hashes(), 1320);
     }
 
+    #[test]
+    fn test_initialise() {
+        let input = test_data();
+        let reader = std::io::Cursor::new(input);
+        let init = Init::parse_init_sequence(reader).unwrap();
+
+        let lens_boxes = init.initialise();
+
+        assert_eq!(lens_boxes.len(), 3);
+        assert_eq!(lens_boxes[&0].len(), 2);
+        assert_eq!(lens_boxes[&3].len(), 3);
+    }
+
+    #[test]
+    fn test_calculate_focusing_power() {
+        let input = test_data();
+        let reader = std::io::Cursor::new(input);
+        let init = Init::parse_init_sequence(reader).unwrap();
+
+        let lens_boxes = init.initialise();
+
+        assert_eq!(Init::calculate_focusing_power(lens_boxes), 145);
+    }
 }
